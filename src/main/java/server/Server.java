8169517dd0b2a1af0920b717f3fc421cda5f01c7
changwenl <changwenl@student.unimelb.edu.au>
2022-10-16 01:57:36 +1100

    try fix connection failure

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -20,12 +20,12 @@ public class Server {
 
 
     public static void main(String[] args) throws IOException, ParseException {
-        System.out.println("Main len: "+args.length);
+        System.out.println("Main len: " + args.length);
         try {
             username = args[0];
             port = Integer.parseInt(args[1]);
             serverIP = InetAddress.getByName(args[2]);
-        } catch (ArrayIndexOutOfBoundsException | UnknownHostException e){
+        } catch (ArrayIndexOutOfBoundsException | UnknownHostException e) {
             System.out.println(e.getStackTrace());
         }
         LinkedBlockingDeque<Socket> socketQueue = new LinkedBlockingDeque<Socket>();
@@ -38,94 +38,15 @@ public class Server {
         }
         ioThread.start();
 
-        while (true){
-            while (!socketQueue.isEmpty()){
+        while (true) {
+            while (!socketQueue.isEmpty()) {
                 System.out.println("Server: received a connection!!!");
                 Socket socket = socketQueue.pop();
                 new ServerReceiveSockets(socket, userList);
-                System.out.println("Server: after execution, userList size: "+userList.size());
+                System.out.println("Server: after execution, userList size: " + userList.size());
                 socket.close();
             }
         }
 
-
-//        ServerSocketFactory factory = ServerSocketFactory.getDefault();
-//        try(ServerSocket server = factory.createServerSocket(port)){
-//            System.out.println(server.getInetAddress());
-//            System.out.println("Waiting for client connection..");
-//            // Wait for connections.
-//            while(true){
-//                Socket client = server.accept();
-//                counter++;
-//                System.out.println("Client "+counter+": Applying for connection!");
-//                // Start a new thread for a connection
-//                Thread t = new Thread(() -> serveClient(client));
-//                t.start();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
-//    private static void serveClient(Socket client)
-//    {
-//        try(Socket clientSocket = client)
-//        {
-//            // The JSON Parser
-//            JSONParser parser = new JSONParser();
-//            // Input stream
-//            DataInputStream input = new DataInputStream(clientSocket.getInputStream());
-//            // Output Stream
-//            DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
-//            System.out.println("CLIENT: "+input.readUTF());
-//            output.writeUTF("Server: Hi Client "+counter+" !!!");
-//            // Receive more data..
-//            while(true){
-//                if(input.available() > 0){
-//                    // Attempt to convert read data to JSON
-//                    JSONObject command = (JSONObject) parser.parse(input.readUTF());
-//                    System.out.println("COMMAND RECEIVED: "+command.toJSONString());
-//                    Integer result = parseCommand(command);
-//                    JSONObject resObj = new JSONObject();
-//                    resObj.put("result", result);
-//                    output.writeUTF(resObj.toJSONString());
-//                }
-//            }
-//        } catch (IOException | ParseException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    private static Integer parseCommand(JSONObject command) {
-//        int result = 0;
-//        if(command.containsKey("command_name")){
-//            System.out.println("IT HAS A COMMAND NAME");
-//        }
-//        if (command.get("command_name").equals("Math"))
-//        {
-//            Math math = new Math();
-//            Integer firstInt = Integer.parseInt(command.get("first_integer").toString());
-//            Integer secondInt = Integer.parseInt(command.get("second_integer").toString());
-//            switch((String) command.get("method_name"))
-//            {
-//                case "add":
-//                    result = math.add(firstInt,secondInt);
-//                    break;
-//                case "multiply":
-//                    result = math.multiply(firstInt,secondInt);
-//                    break;
-//                case "subtract":
-//                    result = math.subtract(firstInt,secondInt);
-//                    break;
-//                default:
-//                    try
-//                    {
-//                        throw new Exception();
-//                    } catch (Exception e) {
-//                        // TODO Auto-generated catch block
-//                        e.printStackTrace();
-//                    }
-//            }
-//        }
-//        // TODO Auto-generated method stub
-//        return result;
-//    }
 }
